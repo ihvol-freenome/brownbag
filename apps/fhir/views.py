bb5b6008ed1e90fb640acbbc9f410155b5d208ed
ihvol-freenome <ihvol.karimov@freenome.com>
2021-09-23 13:58:48 -0700

    plugged DRF to manage FHIR models via REST

--- apps/fhir/views.py
+++ apps/fhir/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.apps import apps
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import GenericModelSerializer
+
+
+
+class FHIRModelView(viewsets.ModelViewSet):
+
+    def get_queryset(self):
+        model = self.get_model(self.kwargs.get("model"))
+        return model.objects.all()
+
+    def get_serializer_class(self):
+        model = self.get_model(self.kwargs.get("model"))
+        GenericModelSerializer.Meta.model = model
+        return GenericModelSerializer
+
+    def get_model(self, name):
+        return apps.get_model(app_label='fhir', model_name=name)
--- brownbag/urls.py
+++ brownbag/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf.urls import include, url
+from rest_framework.routers import DefaultRouter
+from apps.fhir import views
+
+router = DefaultRouter()
+router.register(prefix=r'fhir/(?P<model>[-\w\d]+)', viewset=views.FHIRModelView, basename='fhir')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
 ]
